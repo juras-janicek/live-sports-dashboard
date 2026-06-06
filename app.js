ad2d66e0f8394973f12df863e09a61fbd5d8b946
juras-janicek <juras.janicek@seznam.cz>
2026-06-06 22:24:13 +0200

    added todays matches

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-// app.js - úplný základ
-
-
 
 const API_KEY = '66ec0d1e4bmsh3643c7487adb1cbp1a7cd5jsnf7e1b531f6e2';
 const API_HOST = 'volleyballapi.p.rapidapi.com';
@@ -19,6 +16,11 @@ const options = {
 
 const endpoints = {
   live_matches: '/api/volleyball/matches/live',
+  matches_by_date: {url: '/api/volleyball/matches/',
+                    GetDate(){
+                      return new Date().toLocaleDateString("en-GB")
+                    }
+  },
   categories: '/api/volleyball/tournament/categories',
   league_seasons: '/api/volleyball/unique-tournament/806/seasons',
   league_teams_standing: '/api/volleyball/unique-tournament/806/season/78004/standings/total'
@@ -30,11 +32,45 @@ async function fetchInfo(endpoint) {
     const response = await fetch(`https://${API_HOST}${endpoint}`, options);
     const data = await response.json();
 
-    console.log(data); 
+    console.log(data);
+    return data 
+
   } catch (error) {
     console.error('Chyba:', error);
   }
 }
 
 
-fetchInfo (endpoints.league_teams_standing);
+
+
+
+
+
+
+async function ShowContent(num){
+  const matches = document.getElementById("matches");
+
+  switch(true){
+    case num == 1:
+      document.querySelector(".matches").style.display = "block";
+      document.querySelector(".scoreboard").style.display = "none";
+
+      const data = await fetchInfo(`${endpoints.matches_by_date.url}${endpoints.matches_by_date.GetDate()}`)
+
+      data.events.forEach(event => {
+        matches.innerHTML += `
+          <div class="match">
+            <h4 class="legue" >${event.tournament.name}</h4>
+            <h2 class="game" >${event.homeTeam.name} : ${event.awayTeam.name}</h2>
+          </div>`
+      });
+      break
+    
+    default:
+      document.querySelector(".matches").style.display = "none"
+      document.querySelector(".scoreboard").style.display = "block"
+      break
+    
+  }
+    
+}
